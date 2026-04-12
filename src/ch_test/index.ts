738febcd8acfd769d1ba519d5ff3581429d27d30
vasilii <porokh.vs@gmail.com>
2026-04-12 18:06:35 +0300

    process

--- src/ch_test/index.ts
+++ src/ch_test/index.ts
@@ -63,6 +63,7 @@ animate()
 const mat = new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide })
 const arrowV = createArrow(15, 1)
 const arrow = _M.createMesh({ v: arrowV.v, material: mat })
+//arrow.scale.set(0, 0, -1)
 scene.add(arrow)
 
 const floor = new THREE.GridHelper(100, 10, 0xbbbbbb, 0x808080)
@@ -94,14 +95,10 @@ class Player extends THREE.Object3D {
     dir2: THREE.Object3D
     
     zeroObj: THREE.Object3D
-    camLocXZObjParent: THREE.Object3D
-    camLocXZObj: THREE.Object3D
-    camLocXZObjChild: THREE.Object3D
-    finishObj: THREE.Object3D
+
+    floor: THREE.GridHelper
 
     camArrow: THREE.Mesh
-    camArrowChild: THREE.Object3D
-
 
     _controls: PointerLockControls
 
@@ -111,29 +108,8 @@ class Player extends THREE.Object3D {
     constructor() {
         super()
 
-        const aV = createArrow(5, 1)
-        const am = _M.createMesh({ v: aV.v, material: new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide }) })
-        am.position.set(0, 1, 0)
-        this.add(am)
-
-        const floor = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080)
-        floor.position.y = -1
-        floor.position.x = 0
-        this.add(floor)
-
-        const b = new THREE.Mesh(
-            new THREE.BoxGeometry(.3, .3, .3),
-            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-        )
-        this.add(b)
-
-        const c = new THREE.Mesh(
-            new THREE.CylinderGeometry(.05, .05, 5),
-            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-        )
-        c.position.set(0, 2.5, 0)
-        this.add(c)
-
+        this.floor = new THREE.GridHelper(100, 10, 0x0000ff, 0x808080)
+        this.add(this.floor)
 
         this.dir1 = new THREE.Object3D()
         this.dir1.position.set(0, .5, .5).normalize()
@@ -160,19 +136,13 @@ class Player extends THREE.Object3D {
         scene.add(dirUpArrow)
 
 
-
-
-
         this.zeroObj = new THREE.Object3D()
-
+        
         const camArrowV = createArrow(5, 1)
         this.camArrow = _M.createMesh({ v: camArrowV.v, material: new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }) })
         this.camArrow.scale.z = -1
-        scene.add(this.camArrow)
+        this.zeroObj.add(this.camArrow)
 
-        this.camArrowChild = new THREE.Object3D()
-        this.camArrowChild.position.set(0, 0, 1)
-        this.camArrow.add(this.camArrowChild)
 
         // @ts-ignore
         this._controls = new PointerLockControls(this.camArrow, renderer.domElement)
@@ -191,30 +161,25 @@ class Player extends THREE.Object3D {
             if (e.code === 'KeyA') this.spdLeft = 0
             if (e.code === 'KeyD') this.spdLeft = 0
         })
-
-        this.camLocXZObjParent = new THREE.Object3D()
-        this.camLocXZObj = new THREE.Object3D()
-        this.camLocXZObjParent.add(this.camLocXZObj)
-        this.camLocXZObjChild = new THREE.Object3D()
-        this.camLocXZObjChild.position.z = 1
-        this.camLocXZObj.add(this.camLocXZObjChild)
-
-        this.finishObj = new THREE.Object3D()
     }
 
     update() {
-        //this.translateX(this.spdLeft)
-        //this.translateZ(this.spdForward)
-    
-        this.camArrow.up.copy(this.dirUp.position)
+        this._controls.moveForward(this.spdForward)
+        this._controls.moveRight(this.spdLeft)
+
+        this.zeroObj.up.copy(this.dirUp.position)
+        this.zeroObj.lookAt(this.dir1.position)
+
+        this.floor.quaternion.copy(this.zeroObj.quaternion)
         
-        //this.quaternion.copy(this.camArrow.quaternion)
-        
-        
-        const applyTo = camera        
-        //const applyTo = arrow                
-        applyTo.quaternion.copy(this.camArrow.quaternion)
-        //applyTo.position.copy(this.position)
+        //const applyTo = camera        
+        const applyTo = arrow
+        const wQ = new THREE.Quaternion()
+        this.camArrow.getWorldQuaternion(wQ)
+        applyTo.quaternion.copy(wQ)
+        const p = new THREE.Vector3()
+        this.camArrow.getWorldPosition(p)
+        applyTo.position.copy(p)
     }
 
     lock() {
@@ -235,102 +200,3 @@ const animate2 = () => {
 animate2()
 
 document.addEventListener('click', () => p.lock())
-
-
-
-
-
-
-
-
-
-
-
-
-
-// alert('#$#$#$')
-
-// import { Ticker } from "_CORE/Ticker"
-// import { Keyboard } from "_CORE/Keyboard"
-// import { Phisics } from "_CORE/Phisics"
-// import { ControlsSystem } from "_CORE/controls/controlsWalkOnWalls/ControlsSystem"
-// import { DeviceData } from "_CORE/DeviceData"
-// import { Studio } from "_CORE/Studio"
-// import { LoaderAssets } from "_CORE/Loader"
-
-// import { CONSTANTS, STUDIO_CONF, CONTROLS_CONF, LOAD_ASSETS } from "./constants/CONSTANTS"
-
-// import { Particles } from './entities/Particles'
-
-// import { UiCustom } from "./entities/Ui"
-
-// import { Lab03 } from './EntLab03/Lab03'
-// import { AudioManagerCustom } from "./entities/AudioManagerCustom"
-// import { Materials } from "./entities/Materials"
-// import { pipeInit_04 } from "./pipelines/pipeInit_04"
-// import { pipePlay_04 } from "./pipelines/pipePlay_04"
-// import { pipeEnd_04 } from "./pipelines/pipeEnd_04"
-// import { Core } from '_CORE/types'
-// import { PlayerWallDirection } from "./entities/PlayerWallDirection"
-
-// import "./moveTest"
-
-// // @ts-ignore
-// export interface Root extends Core {
-//     CONSTANTS: typeof CONSTANTS,
-//     ui: UiCustom,
-//     controls: ControlsSystem,
-//     particles: Particles,
-
-//     LOAD_ASSETS: typeof LOAD_ASSETS,
-//     loader: LoaderAssets,
-
-//     lab: Lab03,
-//     audio: AudioManagerCustom,
-//     materials: Materials,
-//     assets: {
-//         [key: string]: any
-//     },
-//     playerWallDirection: PlayerWallDirection,
-// }
-
-
-// // window.addEventListener("DOMContentLoaded", async () => {
-// //     // @ts-ignore:next-line
-// //     console.log("branch:" + __GIT_CURRENT_BRANCH__ + ' commit:' + __HASH_COMMIT__)
-
-// //     const root: Root = {
-// //         CONSTANTS,
-// //         assets: {},
-// //         ticker: new Ticker(),
-// //         studioConf: STUDIO_CONF, 
-// //         studio: new Studio(),
-// //         keyboard: new Keyboard(),
-
-// //         controlsConf: CONTROLS_CONF,
-// //         controls: new ControlsSystem(),
-        
-// //         ui: new UiCustom(),
-// //         particles: new Particles(),
-
-// //         LOAD_ASSETS,
-// //         loader: new LoaderAssets(),
-        
-// //         deviceData: new DeviceData(),
-// //         phisics: new Phisics(),
-// //         lab: new Lab03(),
-
-// //         audioConf: { 
-// //             stepsSpeed: 1 
-// //         },
-// //         audio: new AudioManagerCustom(),
-        
-// //         materials: new Materials(),
-
-// //         playerWallDirection: new PlayerWallDirection()
-// //     }
-
-// //     await pipeInit_04(root)
-// //     await pipePlay_04(root)
-// //     await pipeEnd_04(root)
-// // })
