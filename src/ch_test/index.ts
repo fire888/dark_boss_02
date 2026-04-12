d17b19f2253c63d8a463681f20deb3d656c1ac7d
vasilii <porokh.vs@gmail.com>
2026-04-12 16:48:30 +0300

    process

--- src/ch_test/index.ts
+++ src/ch_test/index.ts
@@ -1,12 +1,13 @@
 import { _M } from '_CORE/_M/_m'
 import * as THREE from 'three';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { createArrow } from '../geometry/arrow/arrow'
 
 const scene = new THREE.Scene()
         
 const camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, .1, 1000)
-camera.position.set(0, 1, 20)
+camera.position.set(30, 20, 0)
 camera.lookAt(new THREE.Vector3(0, 1, -1))
 
 scene.add(camera)
@@ -34,7 +35,7 @@ window.addEventListener( 'resize', onWindowResize)
 onWindowResize()
 
 const axesHelper = new THREE.AxesHelper(10)
-axesHelper.position.set(0, 0, 0)
+axesHelper.position.set(.1, .1, .1)
 scene.add(axesHelper)
 
 const animate = () => {
@@ -53,25 +54,173 @@ animate()
 
 //////////////////////////////////////////////////
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set( 0, 0.5, 0 )
-controls.update()
+// const controls = new OrbitControls(camera, renderer.domElement)
+// controls.target.set( 0, 0.5, 0 )
+// controls.update()
 
 /////////////////////////////////////////////////
 
-const mat = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
-const arrowV = createArrow()
+const mat = new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide })
+const arrowV = createArrow(15, 1)
 const arrow = _M.createMesh({ v: arrowV.v, material: mat })
 scene.add(arrow)
-arrow.up.set(0, 0, 1)
+
+const floor = new THREE.GridHelper(100, 10, 0xbbbbbb, 0x808080)
+floor.position.y = 0
+floor.position.x = 0
+scene.add(floor)
+
+const createBox = (pos: THREE.Vector3) => {
+    const b = new THREE.Mesh(
+        new THREE.BoxGeometry(5, 5, 5),
+        new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
+    )
+    b.position.copy(pos)
+    scene.add(b)
+}
+
+for (let i = 0; i < 1000; i++) {
+    const x = Math.random() * 1000 - 500
+    const y = Math.random() * 1000 - 500
+    const z = Math.random() * 1000 - 500
+    createBox(new THREE.Vector3(x, y, z))
+}
+
+///////////////////////////////////////////////////
+
+class Player extends THREE.Object3D {
+    dirUp: THREE.Object3D
+    dir1: THREE.Object3D
+    dir2: THREE.Object3D
+    
+    zeroObj: THREE.Object3D
+    camLocXZObjParent: THREE.Object3D
+    camLocXZObj: THREE.Object3D
+    camLocXZObjChild: THREE.Object3D
+    finishObj: THREE.Object3D
+
+    camArrow: THREE.Mesh
+    camArrowChild: THREE.Object3D
+
+
+    _controls: PointerLockControls
+
+    spdForward = 0
+    spdLeft = 0
+
+    constructor() {
+        super()
+
+        const aV = createArrow(5, 1)
+        const am = _M.createMesh({ v: aV.v, material: new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide }) })
+        am.position.set(0, 1, 0)
+        this.add(am)
+
+        const floor = new THREE.GridHelper(10, 10, 0x0000ff, 0x808080)
+        floor.position.y = -1
+        floor.position.x = 0
+        this.add(floor)
+
+        const b = new THREE.Mesh(
+            new THREE.BoxGeometry(.3, .3, .3),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        )
+        this.add(b)
+
+        const c = new THREE.Mesh(
+            new THREE.CylinderGeometry(.05, .05, 5),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+        )
+        c.position.set(0, 2.5, 0)
+        this.add(c)
+
+
+        this.dir1 = new THREE.Object3D()
+        this.dir1.position.set(0, 0, 1)
+        this.dir2 = new THREE.Object3D()
+        this.dir2.position.set(-1, 0, 0)
+        this.dirUp = new THREE.Object3D()
+        this.dirUp.position.copy(this.dir1.position).multiply(this.dir2.position)
+
+
+
+
+
+        this.zeroObj = new THREE.Object3D()
+
+        const camArrowV = createArrow(5, 1)
+        this.camArrow = _M.createMesh({ v: camArrowV.v, material: new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide }) })
+        this.camArrow.position.set(0, 1.2, 0)
+        this.add(this.camArrow)
+
+        this.camArrowChild = new THREE.Object3D()
+        this.camArrowChild.position.set(0, 0, 1)
+        this.camArrow.add(this.camArrowChild)
+
+        // @ts-ignore
+        this._controls = new PointerLockControls(this.camArrow, renderer.domElement)
+        this._controls.maxPolarAngle = Math.PI - .01
+        this._controls.minPolarAngle = .01
+
+        document.addEventListener('keydown', (e: KeyboardEvent) => {
+            if (e.code === 'KeyW') this.spdForward = -.5
+            if (e.code === 'KeyS') this.spdForward = .5
+            if (e.code === 'KeyA') this.spdLeft = -.5
+            if (e.code === 'KeyD') this.spdLeft = .5
+        })
+        document.addEventListener('keyup', (e: KeyboardEvent) => {
+            if (e.code === 'KeyW') this.spdForward = 0
+            if (e.code === 'KeyS') this.spdForward = 0
+            if (e.code === 'KeyA') this.spdLeft = 0
+            if (e.code === 'KeyD') this.spdLeft = 0
+        })
+
+        this.camLocXZObjParent = new THREE.Object3D()
+        this.camLocXZObj = new THREE.Object3D()
+        this.camLocXZObjParent.add(this.camLocXZObj)
+        this.camLocXZObjChild = new THREE.Object3D()
+        this.camLocXZObjChild.position.z = 1
+        this.camLocXZObj.add(this.camLocXZObjChild)
+
+        this.finishObj = new THREE.Object3D()
+    }
+
+    update() {
+        this.translateX(this.spdLeft)
+        this.translateZ(this.spdForward)
+    
+        this.camArrow.up.copy(this.dirUp.quaternion)
+        
+        this.quaternion.copy(this.camArrow.quaternion)
+        
+        
+        //const applyTo = camera        
+        const applyTo = arrow                
+        applyTo.quaternion.copy(this.quaternion)
+        applyTo.position.copy(this.position)
+    }
+
+    lock() {
+        this._controls.lock()
+    }
+    
+    
+}
+
+const p = new Player()
+p.position.set(15, 0, 15)
+scene.add(p)
 
 const animate2 = () => {
     requestAnimationFrame(animate2)
-    arrow.rotation.y += 0.01
+    p.update()
 }
 animate2()
 
-///////////////////////////////////////////////////
+document.addEventListener('click', () => p.lock())
+
+
+
 
 
 
--- src/geometry/arrow/arrow.ts
+++ src/geometry/arrow/arrow.ts
@@ -1,22 +1,23 @@
-export const createArrow = (): { v: number[] } => {
-    const w = .25
+export const createArrow = (s: number = 10, wA: number = .2): { v: number[] } => {
+    const w = wA * .2
     const wH = w * .5
-    const d = 10
-    const w2 = w * 5
+    const d = .7 * s
+    const w2 = wA
     const wH2 = w2 * .5
-    const d2 = 5
+    const d2 = .3 * s
+
     const v = [
-        -wH, 0, 0,
-        wH, 0, 0,
-        wH, 0, -d,
+         -wH,   0,   0,
+          wH,   0,   0,
+          wH,   0,   d,
 
-        -wH, 0, 0,
-        wH, 0, -d,
-        -wH, 0, -d,
+         -wH,   0,   0,
+          wH,   0,   d,
+         -wH,   0,   d,
 
-        -wH2, 0, -d,
-        wH2, 0, -d,
-        0, 0, -d2 - d,
+        -wH2,   0,   d,
+         wH2,   0,   d,
+           0,   0,   d2 +d,
     ]
 
     return { v }
