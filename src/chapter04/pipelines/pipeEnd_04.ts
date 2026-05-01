534f8405207a8262aeadf37897d5a529f1e1bc2e
vasilii <porokh.vs@gmail.com>
2026-05-01 15:58:47 +0300

    [ch04]: pipeEnd

--- src/chapter04/pipelines/pipeEnd_04.ts
+++ src/chapter04/pipelines/pipeEnd_04.ts
@@ -1,14 +1,12 @@
 import { Root } from "../index"
 
 export const pipeEnd_04 = async (root: Root) => {
-    const {
-        studio,
-        ui,
-    } = root
+    const { studio, ui, controls } = root
 
     await studio.animateFog({ endFogNear: .3, endFogFar: .5 , time: 5000 })
 
-    root.controls.disable()
+    controls.disable()
     setTimeout(() => ui.toggleControlsArrows(false), 500)
+    
     await ui.showFinalPage()
 }
