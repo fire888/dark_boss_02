b2a2593ba44f5c88af26964082893c08a048e0f8
vasilii <porokh.vs@gmail.com>
2026-07-06 11:36:06 +0300

    [sh03]: scheme

--- src/chapter03/entities/Lab03/InnerLab/InnerLab.ts
+++ src/chapter03/entities/Lab03/InnerLab/InnerLab.ts
@@ -36,7 +36,6 @@ export class InnerLab {
 
         const r = room00()
         _M.translateVertices(r.v, 0, 4, D - 30 - 15)
-        // _M.translateVertices(r.v, 0, 4, 0)
         _M.mergeIndexedArrays(g, r)
 
         this.mesh = _M.createMesh({ ...g, material: materials.levelMatNorm })
@@ -51,52 +50,52 @@ export class InnerLab {
     }
 
     #calcLevel() {
-       const sh = new Scheme()
-       console.log(sh.scheme)
+        const sh = new Scheme()
+        //console.log(sh.scheme)
 
 
-       const v: number[] = []
+        const v: number[] = []
 
-       const H = 15
+        const H = 60
+
+        const geomBox = new THREE.BoxGeometry(1, 1, 1)
+        const mat = new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            side: 2
+        }) 
 
 
-       const geomBox = new THREE.BoxGeometry(1, 1, 1)
-       const mat = new THREE.MeshBasicMaterial({
-           color: 0xffffff,
-           side: 2
-       }) 
-       for (let i = 0; i < sh.scheme.length; i++) {
-            const branch = sh.scheme[i]
-            for (let j = 1; j < branch.length; j++) {
-                const vPrev = branch[j - 1]
-                const vCurr = branch[j]
-                const dir = new THREE.Vector3().copy(vCurr).sub(vPrev).normalize()
-                const p0 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI * .5).add(vPrev) 
-                const p1 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).add(vPrev) 
-                const p2 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).add(vCurr) 
-                const p3 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI * .5).add(vCurr)
-                
-                const _v = _M.createPolygonV(p0, p1, p2, p3)
-                //console.log(_v)
-                _M.fill(_v, v)
+        for (let key in sh.scheme.lines) {
+            const { p0, p1, dist } = sh.scheme.lines[key]
 
-                const l = _M.createLabel('' + i, [1, 0, 0], 10)
-                l.position.copy(vCurr).add(new THREE.Vector3(0, H, 0))
-                this._root.studio.add(l)
+            const vPrev = sh.scheme.points[p0]
+            const vCurr = sh.scheme.points[p1]
+
+            if (!vPrev || !vCurr) {
+                console.warn('Invalid line:', key, sh.scheme.lines[key])
+                continue
             }
 
+            const l = _M.createLabel(p1, [1, 0, 0], 10)
+            l.position.copy(vCurr.pos).add(new THREE.Vector3(0, H, 0))
+            this._root.studio.add(l)
 
+            
+            const dir = new THREE.Vector3().copy(vCurr.pos).sub(vPrev.pos).normalize()
+            const w = .1
+            const vp0 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI * .5).multiplyScalar(w).add(vPrev.pos) 
+            const vp1 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).multiplyScalar(w).add(vPrev.pos) 
+            const vp2 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5).multiplyScalar(w).add(vCurr.pos) 
+            const vp3 = dir.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI * .5).multiplyScalar(w).add(vCurr.pos)
 
+            const _v = _M.createPolygonV(vp0, vp1, vp2, vp3)
+            _M.fill(_v, v)
+        }
 
+        const mesh = _M.createMesh({ v, material: mat })
+        mesh.position.set(0, H, 0)
+        this._root.studio.add(mesh)
 
-           //const mesh = new THREE.Mesh(geomBox, mat)
-           //mesh.position.copy(v3)
-           //this._root.studio.add(mesh)
-       }
-
-       const mesh = _M.createMesh({ v, material: mat })
-       mesh.position.set(0, H, 0)
-       this._root.studio.add(mesh)
 
 
     }
--- src/chapter03/entities/Lab03/InnerLab/Scheme.ts
+++ src/chapter03/entities/Lab03/InnerLab/Scheme.ts
@@ -1,48 +1,213 @@
 import * as THREE from 'three'
 
+type IPoint = { 
+    points: { 
+        [key: string]: { 
+            pos: THREE.Vector3,
+            neighbors?: string[]
+        }
+    },
+    lines: { 
+        [key: string]: {
+            p0: string, 
+            p1: string, 
+            dist: number 
+        } 
+    } 
+}
+
 export class Scheme {
-    scheme: THREE.Vector3[][] = []
+    scheme: IPoint
     constructor() {
+        const BOUNDS = new THREE.Box3(
+            new THREE.Vector3(-50, -50, -50), 
+            new THREE.Vector3(50, 50, 50)
+        )
 
-        let MAX_BRANCHES = 100
+        const agent = {
+            pos: new THREE.Vector3(0, 0, 0),
+            dir: new THREE.Vector3(0, 0, -1),
+            currentPointId: 'p0'
+        }
 
-        const makeBranch = (v3Start: THREE.Vector3, v3Dir: THREE.Vector3, num: number) => {
-            MAX_BRANCHES--
-            if (MAX_BRANCHES < 0) return
+        const SH: IPoint = { 
+            points: {
+                p0: { 
+                    pos: agent.pos.clone(), 
+                    neighbors: [] 
+                },
+            }, 
+            lines: {} 
+        }
 
-            const v3 = v3Start.clone()
-            const v3CurrDir = v3Dir.clone() 
+        let MAX_POINTS = 400
+        let count = 0
 
-            const branch: THREE.Vector3[] = [ v3.clone() ]
+        const agentTryFindNewPoint = (maxAngle: number = Math.PI * .5) => {
+            const dist = Math.random() * 5 + 5
+            const h = Math.random() * dist * .5 * (Math.random() < .5 ? 1 : -1)
 
-            for (let i = 0; i < num; i++) {
-                const addDir = new THREE.Vector3(Math.random() -.5, Math.random() * .1 - .05, Math.random() -.5)
-                v3CurrDir.add(addDir)
+            const dir = 
+                agent.dir.clone()
+                    .applyAxisAngle(
+                        new THREE.Vector3(0, 1, 0), 
+                        Math.random() * maxAngle * (Math.random() < .5 ? 1 : -1)
+                    )
+            const newPos = agent.pos.clone()
+                .add(dir.clone().multiplyScalar(dist))
+                .add(new THREE.Vector3(0, h, 0))
 
-                const d = Math.random() * 5 + 5
-                v3.add(v3CurrDir.clone().multiplyScalar(d))
-                branch.push(v3.clone())
+            return { newPos, dir, dist }
+        }
 
-                if (Math.random() < .3) {
-                    const dir = v3CurrDir
-                        .clone()
-                        .applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5 * Math.random() < .5 ? 1 : -1)
+        const calkNextPoint = () => {
+            const maxAttempts = 10
 
-                    makeBranch(v3, dir, Math.floor(Math.random() * 10) + 2)
+            let count = 0
+            while (count < maxAttempts) { 
+                ++count
+
+                let { newPos, dir, dist } = agentTryFindNewPoint(count / maxAttempts * Math.PI * 4)
+
+                if (BOUNDS.containsPoint(newPos)) {
+                    return { newPos, dir, dist }
+                } else {
+                    //  console.log('try findPoint: count:', count, 'newPos:', newPos)
                 }
             }
 
-            this.scheme.push(branch)
-
-            //return branch
+            return null
         }
 
+        let CC_ID = 0
+        const fillBranch = (MAX_POINTS: number) => {
+            let count = 0
+            while (count < MAX_POINTS) {
+                ++count
+                ++CC_ID
+
+                const nextPoint = calkNextPoint()
+                if (nextPoint) { 
+                    let { newPos, dir, dist } = nextPoint
+                    let newPointId = `p${CC_ID}`
+
+                    // 
+                    for (let key in SH.points) {
+                        const p = SH.points[key]
+                        if (newPos.distanceTo(p.pos) < 3) { 
+                            newPointId = key
+                            newPos = p.pos.clone()
+                            break
+                        }
+                    }
+
+                    if (!SH.points[newPointId]) {
+                        SH.points[newPointId] = { 
+                            pos:newPos.clone(),
+                            neighbors: [] 
+                        }
+                    }
+                    SH.lines[`l${CC_ID}`] = { p0: agent.currentPointId, p1: newPointId, dist }
+
+                    agent.pos.copy(newPos)
+                    agent.dir.copy(dir)
+                    agent.currentPointId = newPointId
+                } else {
+                    count = MAX_POINTS + 1
+                }
+            }
+
+            // fill neighbors
+            const lines = SH.lines
+            for (let key in lines) {
+                const l = lines[key]
+                if (l) {
+                    const { p0, p1 } = l
+                    if (
+                        SH.points[p0] && 
+                        SH.points[p0].neighbors && 
+                        !SH.points[p0].neighbors.includes(p1)
+                    ) { 
+                        SH.points[p0].neighbors?.push(p1) 
+                    }
+
+                    if (
+                        SH.points[p1] && 
+                        SH.points[p1].neighbors && 
+                        !SH.points[p1].neighbors.includes(p0)
+                    ) { 
+                        SH.points[p1].neighbors?.push(p0) 
+                    }
+                }
+            }
+        }
+
+        fillBranch(20)
+
+        //let cc = 0
+        for (let key in SH.points) { 
+            if (Math.random() < 1.1 && SH.points[key].neighbors?.length === 2) {
+                // if (cc !== 0) {
+                //     break
+                // }
+                // ++cc
+
+                console.log('branch')
+                const posP1 = SH.points[SH.points[key].neighbors![0]].pos.clone()
+                const posP2 = SH.points[SH.points[key].neighbors![1]].pos.clone()
+
+                posP2.sub(posP1).normalize().applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5)
+                const dir = posP2.clone()
+                console.log('dir:', dir)
+
+                agent.pos.copy(SH.points[key].pos.clone())
+                agent.dir.copy(dir)
+
+                fillBranch(10)
+            }
+        }
+
+        console.log('Scheme generated:', SH)
+
+        this.scheme = SH
+
+        // const makeBranch = (v3Start: THREE.Vector3, v3Dir: THREE.Vector3, num: number) => {
+        //     MAX_BRANCHES--
+        //     if (MAX_BRANCHES < 0) return
+
+        //     const v3 = v3Start.clone()
+        //     const v3CurrDir = v3Dir.clone() 
+
+        //     const branch: THREE.Vector3[] = [ v3.clone() ]
+
+        //     for (let i = 0; i < num; i++) {
+        //         const addDir = new THREE.Vector3(Math.random() -.5, Math.random() * .1 - .05, Math.random() -.5)
+        //         v3CurrDir.add(addDir)
+
+        //         const d = Math.random() * 5 + 5
+        //         v3.add(v3CurrDir.clone().multiplyScalar(d))
+        //         branch.push(v3.clone())
+
+        //         if (Math.random() < .3) {
+        //             const dir = v3CurrDir
+        //                 .clone()
+        //                 .applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * .5 * Math.random() < .5 ? 1 : -1)
+
+        //             makeBranch(v3, dir, Math.floor(Math.random() * 10) + 2)
+        //         }
+        //     }
+
+        //     this.scheme.push(branch)
+
+        //     //return branch
+        // }
 
 
-        const v3 = new THREE.Vector3(0, 0, 0)
-        const currDir = new THREE.Vector3(0, 0, -1)
 
-        makeBranch(v3, currDir, 20)
+        // const v3 = new THREE.Vector3(0, 0, 0)
+        // const currDir = new THREE.Vector3(0, 0, -1)
+
+        // makeBranch(v3, currDir, 20)
 
     }
 }   
